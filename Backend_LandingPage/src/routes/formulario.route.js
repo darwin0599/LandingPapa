d6d7bd416b4c26c0abe57016f1dcb3b619906ec5
darwin0599 <72778324+darwin0599@users.noreply.github.com>
2021-11-30 14:46:37 -0500

    Delete Backend_LandingPage directory

--- Backend_LandingPage/src/routes/formulario.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express')();
-const formularioController = require('../controllers/formulario.controller');
-const formularioSchemaMiddleware = require('../middleware/formulario_schema.middleware');
-const formularioDocMiddleware = require('../middleware/formulario_documento.middleware');
-
-router.post('/', formularioSchemaMiddleware ,formularioDocMiddleware ,formularioController);
-
-module.exports = router;
--- Backend_LandingPage/tablaFormulario.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE formPapaDeLosRegalos(
-	id integer auto_increment,
-	nombre varchar(100) NOT NULL,
-	documento  varchar(10) NOT NULL,
-	email varchar(100) NOT NULL,
-	primary key (id)
-)
